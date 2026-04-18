d377e13e4f6932299490af5cb0c81dda618ec1d9
patty1xd <alexandruflorea2019@gmail.com>
2026-04-19 00:48:38 +0300

    Update PlayerJoinListener.java

--- src/main/java/com/ffa/listeners/PlayerJoinListener.java
+++ src/main/java/com/ffa/listeners/PlayerJoinListener.java
@@ -3,6 +3,7 @@ package com.ffa.listeners;
 import com.ffa.FFAPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -17,6 +18,7 @@ public class PlayerJoinListener implements Listener {
         var player = event.getPlayer();
         plugin.getTierManager().initPlayer(player.getUniqueId());
         plugin.getBoardManager().updatePlayer(player);
+        plugin.getBoardManager().updateNameTag(player);
         player.sendMessage("§8[§6FFA§8] §7Welcome! Right-click the §eKit §7NPC to get your kit.");
         player.sendMessage("§8[§6FFA§8] §7Your tier: " + plugin.getTierManager().getTierDisplay(plugin.getTierManager().getTier(player.getUniqueId())));
     }
@@ -24,5 +26,14 @@ public class PlayerJoinListener implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         plugin.getTierManager().saveAll();
+    }
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+        event.setCancelled(true);
+        var player = event.getPlayer();
+        String tierDisplay = plugin.getTierManager().getTierDisplay(plugin.getTierManager().getTier(player.getUniqueId()));
+        String formatted = tierDisplay + " §f" + player.getName() + " §7» §f" + event.getMessage();
+        player.getServer().broadcastMessage(formatted);
     }
 }
