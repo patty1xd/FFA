29545ba8b3c4497a74d3271cd690146859e800c0
patty1xd <alexandruflorea2019@gmail.com>
2026-04-19 14:49:40 +0300

    Update PlayerKillListener.java

--- src/main/java/com/ffa/listeners/PlayerKillListener.java
+++ src/main/java/com/ffa/listeners/PlayerKillListener.java
@@ -1,6 +1,7 @@
 package com.ffa.listeners;
 
 import com.ffa.FFAPlugin;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -18,14 +19,21 @@ public class PlayerKillListener implements Listener {
         Player victim = event.getEntity();
         Player killer = victim.getKiller();
 
+        String victimTier = plugin.getTierManager().getTierDisplay(plugin.getTierManager().getTier(victim.getUniqueId()));
+
         // Victim drops a tier
         plugin.getTierManager().onDeath(victim.getUniqueId());
 
-        // Killer gains a kill
         if (killer != null && killer != victim) {
+            String killerTier = plugin.getTierManager().getTierDisplay(plugin.getTierManager().getTier(killer.getUniqueId()));
             plugin.getTierManager().addKill(killer.getUniqueId());
-            String tierDisplay = plugin.getTierManager().getTierDisplay(plugin.getTierManager().getTier(killer.getUniqueId()));
-            killer.sendMessage("§8[§6FFA§8] §eYou killed §f" + victim.getName() + "§e! " + tierDisplay);
+
+            // Custom death message
+            event.setDeathMessage(
+                killerTier + " §f" + killer.getName() + " §7slew " + victimTier + " §f" + victim.getName()
+            );
+        } else {
+            event.setDeathMessage(victimTier + " §f" + victim.getName() + " §7died.");
         }
     }
 }
