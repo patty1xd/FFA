548104062026827a832350325d678baf8607c812
patty1xd <alexandruflorea2019@gmail.com>
2026-04-21 19:38:12 +0300

    Update FFAPlugin.java

--- src/main/java/com/ffa/FFAPlugin.java
+++ src/main/java/com/ffa/FFAPlugin.java
@@ -3,6 +3,10 @@ package com.ffa;
 import com.ffa.commands.*;
 import com.ffa.listeners.*;
 import com.ffa.managers.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FFAPlugin extends JavaPlugin {
@@ -12,6 +16,9 @@ public class FFAPlugin extends JavaPlugin {
     private NPCManager npcManager;
     private KitManager kitManager;
     private ScoreboardManager scoreboardManager;
+    private GearEffectManager gearEffectManager;
+    private NormalizationManager normalizationManager;
+    private ChatManager chatManager;
 
     @Override
     public void onEnable() {
@@ -22,29 +29,49 @@ public class FFAPlugin extends JavaPlugin {
         npcManager = new NPCManager(this);
         kitManager = new KitManager(this);
         scoreboardManager = new ScoreboardManager(this);
+        gearEffectManager = new GearEffectManager(this);
+        normalizationManager = new NormalizationManager(this);
+        chatManager = new ChatManager(this);
 
         getServer().getPluginManager().registerEvents(new PlayerKillListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
         getServer().getPluginManager().registerEvents(new NPCInteractListener(this), this);
         getServer().getPluginManager().registerEvents(new NPCProtectListener(this), this);
+        getServer().getPluginManager().registerEvents(new GearEffectListener(this), this);
+        getServer().getPluginManager().registerEvents(normalizationManager, this);
 
         getCommand("spawnnpc").setExecutor(new SpawnNPCCommand(this));
         getCommand("removenpc").setExecutor(new RemoveNPCCommand(this));
         getCommand("tier").setExecutor(new TierCommand(this));
         getCommand("settier").setExecutor(new SetTierCommand(this));
         getCommand("resetplayer").setExecutor(new ResetPlayerCommand(this));
+        getCommand("koalaffa").setExecutor((sender, cmd, label, args) -> {
+            if (!sender.hasPermission("ffa.admin")) { sender.sendMessage("§cNo permission."); return true; }
+            if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+                reloadConfig(); sender.sendMessage("§aKoalaFFA config reloaded!"); return true;
+            }
+            sender.sendMessage("§cUsage: /koalaffa reload"); return true;
+        });
+
+        // /msg and /tell commands
+        MsgCommand msgCmd = new MsgCommand(this);
+        getCommand("msg").setExecutor(msgCmd);
+        getCommand("tell").setExecutor(msgCmd);
+        ReplyCommand replyCmd = new ReplyCommand(this);
+        getCommand("reply").setExecutor(replyCmd);
+        getCommand("r").setExecutor(replyCmd);
 
         npcManager.restoreNPC();
         scoreboardManager.startUpdater();
 
-        getLogger().info("FFAKit enabled!");
+        getLogger().info("KoalaFFA enabled!");
     }
 
     @Override
     public void onDisable() {
         if (tierManager != null) tierManager.saveAll();
         if (npcManager != null) npcManager.removeNPC();
-        getLogger().info("FFAKit disabled.");
+        getLogger().info("KoalaFFA disabled.");
     }
 
     public static FFAPlugin getInstance() { return instance; }
@@ -52,4 +79,7 @@ public class FFAPlugin extends JavaPlugin {
     public NPCManager getNPCManager() { return npcManager; }
     public KitManager getKitManager() { return kitManager; }
     public ScoreboardManager getBoardManager() { return scoreboardManager; }
+    public GearEffectManager getGearEffectManager() { return gearEffectManager; }
+    public NormalizationManager getNormalizationManager() { return normalizationManager; }
+    public ChatManager getChatManager() { return chatManager; }
 }
