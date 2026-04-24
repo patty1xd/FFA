93cd6ea972fbf8f6eaa03e85e64f24fcb2c442b2
patty1xd <alexandruflorea2019@gmail.com>
2026-04-24 23:29:51 +0300

    Update FFAPlugin.java

--- src/main/java/com/ffa/FFAPlugin.java
+++ src/main/java/com/ffa/FFAPlugin.java
@@ -14,6 +14,8 @@ public class FFAPlugin extends JavaPlugin {
     private ScoreboardManager scoreboardManager;
     private NormalizationManager normalizationManager;
     private ChatManager chatManager;
+    private StatsManager statsManager;
+    private HologramManager hologramManager;
 
     @Override
     public void onEnable() {
@@ -26,11 +28,14 @@ public class FFAPlugin extends JavaPlugin {
         scoreboardManager = new ScoreboardManager(this);
         normalizationManager = new NormalizationManager(this);
         chatManager = new ChatManager(this);
+        statsManager = new StatsManager(this);
+        hologramManager = new HologramManager(this);
 
         getServer().getPluginManager().registerEvents(new PlayerKillListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
         getServer().getPluginManager().registerEvents(new NPCInteractListener(this), this);
         getServer().getPluginManager().registerEvents(new NPCProtectListener(this), this);
+        getServer().getPluginManager().registerEvents(new GearEffectListener(this), this);
         getServer().getPluginManager().registerEvents(normalizationManager, this);
 
         getCommand("spawnnpc").setExecutor(new SpawnNPCCommand(this));
@@ -41,10 +46,14 @@ public class FFAPlugin extends JavaPlugin {
         getCommand("koalaffa").setExecutor((sender, cmd, label, args) -> {
             if (!sender.hasPermission("ffa.admin")) { sender.sendMessage("§cNo permission."); return true; }
             if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
-                reloadConfig(); sender.sendMessage("§aKoalaFFA config reloaded!"); return true;
+                reloadConfig(); sender.sendMessage("§aKoalaFFA reloaded!"); return true;
             }
             sender.sendMessage("§cUsage: /koalaffa reload"); return true;
         });
+
+        StatsCommand statsCmd = new StatsCommand(this);
+        getCommand("stats").setExecutor(statsCmd);
+        getCommand("hologram").setExecutor(statsCmd);
 
         MsgCommand msgCmd = new MsgCommand(this);
         getCommand("msg").setExecutor(msgCmd);
@@ -62,6 +71,7 @@ public class FFAPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         if (tierManager != null) tierManager.saveAll();
+        if (statsManager != null) statsManager.saveAll();
         if (npcManager != null) npcManager.removeNPC();
         getLogger().info("KoalaFFA disabled.");
     }
@@ -73,4 +83,6 @@ public class FFAPlugin extends JavaPlugin {
     public ScoreboardManager getBoardManager() { return scoreboardManager; }
     public NormalizationManager getNormalizationManager() { return normalizationManager; }
     public ChatManager getChatManager() { return chatManager; }
+    public StatsManager getStatsManager() { return statsManager; }
+    public HologramManager getHologramManager() { return hologramManager; }
 }
